3f5d7ec913993c52e44ca247f72f3a70a1b86246
W0781459 <W0781459@ADL-A1201I-24>
2023-11-01 13:32:11 -0400

    Modify the namespace to namespace LizBookStore.Area.Customer.Controllers

--- LizBookStore/Areas/Customer/Controllers/HomeController.cs
+++ LizBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LizBookStore.Controllers
+namespace LizBookStore.Area.Customer.Controllers
 
 {
     [Area("Customer")]

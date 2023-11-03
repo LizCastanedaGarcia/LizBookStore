f166904d946e5ffc5ddb674ac4c9e697cdeb9be2
W0781459 <W0781459@ADL-A1201G-23>
2023-11-03 14:15:46 -0400

    Add new code in IRepository.cs

--- LizBook.DataAccess/Repository/IRepository/IRepository.cs
+++ LizBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -16,6 +16,17 @@ namespace LizBook.DataAccess.Repository.IRepository
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             string includeProperties = null
             );
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+        void Add(T entity); //to add an entity
+
+        void Remove(int id); //to remove an object or category
+
+        void Remove(T entity);  //another way to remove an object
+
+        void RemoveRange(IEnumerable<T> entity);  //removes a complete range of entities
 
     }
 }

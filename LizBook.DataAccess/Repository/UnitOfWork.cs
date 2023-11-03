cf5e7e6a02a7e7bea9bef4d67f9fc0ebddf816c3
W0781459 <W0781459@WDL-A0336Z-16>
2023-11-03 17:22:37 -0400

    update code in UnitOfWork.cs

--- LizBook.DataAccess/Repository/UnitOfWork.cs
+++ LizBook.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LizBook.DataAccess.Repository.IRepository;
+using LizBookStore.DataAccess.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace LizBook.DataAccess.Repository
 {
-    class UnitOfWork
+    public class UnitOfWork 
     {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public ISP_Call SP_Call { get; private set; }
     }
 }

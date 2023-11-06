cd904bb65315a28e18b9111e5e40cd50216ca640
W0781459 <W0781459@ADL-A1201G-19>
2023-11-06 08:56:12 -0500

    Add a new folder in Areas/View with the name Category

---
+++ LizBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using LizBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LizBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //API calls here
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //return NotFound();
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
